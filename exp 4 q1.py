32f88618793b72789faeee7131199bb06c2c4121
sumbul hashmi <sumbulhashmi726@gmail.com>
2026-02-28 09:25:04 +0530

    Add files via upload

---
+++ exp 4 q1.py
@@ -0,0 +1,10 @@
+# Program to count capital letters in a string
+
+string = input("Enter a string: ")
+count = 0
+
+for ch in string:
+    if ch.isupper():
+        count += 1
+
+print("Number of capital letters:", count)
